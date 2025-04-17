c5f3d771c43ecd7670e7e182d016f5d7c14c465f
Nolan Biscaro <biscaronolan@gmail.com>
2025-04-17 14:11:17 -0700

    update imports

---
+++ rust/moonlink_connectors/src/lib.rs
@@ -0,0 +1,4 @@
+pub mod pg_replicate;
+mod postgres;
+
+pub use postgres::Sink;
---
+++ rust/pg_moonlink_test/src/main.rs
@@ -0,0 +1,33 @@
+use moonlink_connectors::pg_replicate::pipeline::{
+    batching::{data_pipeline::BatchDataPipeline, BatchConfig},
+    sources::postgres::{PostgresSource, TableNamesFrom},
+    PipelineAction,
+};
+use moonlink_connectors::Sink;
+use std::{error::Error, path::PathBuf, time::Duration};
+
+async fn main_impl() -> Result<(), Box<dyn Error>> {
+    let source = PostgresSource::new(
+        "localhost",
+        5432,
+        "postgres",
+        "postgres",
+        Some("password".to_string()),
+        Some("moonlink_slot".to_string()),
+        TableNamesFrom::Publication("moonlink_pub".to_string()),
+    )
+    .await?;
+    let sink = Sink::new(None, PathBuf::from("/home/vscode/moonlink_test/"));
+    let batch_config = BatchConfig::new(1000, Duration::from_secs(1));
+    let mut pipeline = BatchDataPipeline::new(source, sink, PipelineAction::Both, batch_config);
+    pipeline.start().await?;
+    Ok(())
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn Error>> {
+    if let Err(e) = main_impl().await {
+        eprintln!("{e}");
+    }
+    Ok(())
+}
